c8558a11c0c5aeeb18c7c6594cfcf92c5924435c
Aisha Abdiweli <aishaabdiweli25@gmail.com>
2026-02-19 00:30:40 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Greeting from "./components/Greetings";
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      {/* <Greeting name = "Aisha" surname = "Abdiweli"  /> */}
+      <Header />
+      
+    </>
+  );
+}
+
+export default App;
+    
